f1b0cf76d7ae551534b20023a61d3e96e50cc047
TungPT12 <mylifeundermycontrol@gmail.com>
2023-03-23 20:02:31 +0700

    delete danhs gias

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -107,27 +107,6 @@
                 </div>
             </div>
         </div>
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="d-flex no-block align-items-center">
-                            <div>
-                                <h3><i class="icon-bag"></i></h3>
-                                <p class="text-muted">Đánh giá</p>
-                                <p>({{ $month . date('-Y') }})</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12">
-                        <div class="progress">
-                            <div class="progress-bar bg-success" role="progressbar" style="width: 85%; height: 6px;"
-                                aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
     <div class="row">
         <div class="col-lg-12">
